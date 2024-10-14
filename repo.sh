43e229e92c1d73933b0c6a6665129a19c1c8cd02
NR4U <121803629+NR4U@users.noreply.github.com>
2024-10-14 13:37:22 +0300

    repo.sh

--- repo.sh
+++ repo.sh
@@ -5,21 +5,21 @@ cd $script_full_path || exit 1
 rm Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
 
 echo "[Repository] Generating Packages..."
-apt-ftparchive packages ./pool > Packages
+apt-ftparchive packages ./debs > Packages
 zstd -q -c19 Packages > Packages.zst
 xz -c9 Packages > Packages.xz
 bzip2 -c9 Packages > Packages.bz2
 
 echo "[Repository] Generating Release..."
 apt-ftparchive \
-		-o APT::FTPArchive::Release::Origin="MohaRepo" \
-		-o APT::FTPArchive::Release::Label="MohaRepo" \
+		-o APT::FTPArchive::Release::Origin="RivesDev" \
+		-o APT::FTPArchive::Release::Label="RivesDev" \
 		-o APT::FTPArchive::Release::Suite="stable" \
 		-o APT::FTPArchive::Release::Version="1.0" \
 		-o APT::FTPArchive::Release::Codename="ios" \
 		-o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64" \
 		-o APT::FTPArchive::Release::Components="main" \
-		-o APT::FTPArchive::Release::Description="MohaRepo Priv8" \
+		-o APT::FTPArchive::Release::Description="Rives Dev" \
 		release . > Release
 
 echo "[Repository] Signing Release"
